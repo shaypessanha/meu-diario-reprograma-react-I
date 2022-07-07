c568d5ba8041b58441204a1ddbcdffab8bbd5eef
shaypessanha <shayannepessanha@gmail.com>
2022-07-06 23:39:56 -0300

    ajeitei css

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Card Title="HTML/CSS" Image="./imgs/handsup.gif" Text="HTML e CSS foram muito divertidos e faceis de aprender"></Card>
           <Card Title="Git" Image="./imgs/fineloveyou.gif" Text="Foi desesperador no começo, mas no final deu tudo certo"></Card>
           <Card Title="JavaScript" Image="./imgs/tried_and_failed.gif" Text="Estou até agora tentando entender..."></Card>
-          <Card Title="React" Image="./imgs/clapping1.gif" Text="O que dizer do React que mal conheço, mas já considero pacas"></Card>
+          <Card Title="React" Image="./imgs/clapping1.gif" Text="O que dizer do React que mal conheço, mas já considero pakas"></Card>
           <Card Title="Bônus: Meus entregáveis da semana" Image="./imgs/shooting_cringe.gif" Text="É tiro pra todo lado: uma hora dá certo"></Card>
         </div>
       </div>
